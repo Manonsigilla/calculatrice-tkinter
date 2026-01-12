1302b87c47543f8e6094c370460519eff940bf0f
Manon Sigaud <sigaudmanon@gmail.com>
2026-01-12 13:23:47 +0100

    Structure de base

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+# src/__init__.py
+"""
+Package calculatrice - Calculatrice avec historique et validation
+Version: 1.0.0
+"""
+
+# Fichier vide - sert juste à marquer le dossier comme package Python
